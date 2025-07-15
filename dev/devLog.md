1c45a8b7eccddc1a6ae5b65be20bf85cfd22df13
Dhyan Joshi <dhyanjoshi544@gmail.com>
2025-07-15 23:41:24 +0530

    feat: added perfect and genius pannning effect

--- dev/devLog.md
+++ dev/devLog.md
@@ -40,3 +40,47 @@ let do some shit where we have 100 to 200 range and we show 10 horizontal line w
 well that was easy just declared the price range, defined the grid with and added step size
 I think the main complexity will be when we have dynamic infinite scroll
 
+3. now to implement scroll I got general idea
+
+Mouse down - get the position
+Mouse move - continue tracking position
+but we track position in pixel and our axis are in price and time 
+ok with quick search I found that we will need scale for both price and time which are relatively easy h / range and w / range
+one more thing to note change onMouseUp from canvas to window because if user clicks on canvas and ups outside of the canvas it doesn't release the drag which is not good UX
+ok now the functions are setup and I get the distances, if i go left and top I will get positive distance and if i go right and down I will get minus position, now if I am right I just have to get new priceScale and multiply and add all the data to min max time and price, let's see.
+I added panning and created effect and told it to draw whenever we change our min max time and price and result
+![panning_one](first_panning.png)
+yup, one and only your uncrowned king did it again
+now I just need to make sure to clear canvas before each draw
+
+ok but everything is going too fast even if do just bit of panning that's not great, what the fuck is going on
+ohhhh, make sense.
+so apparently I was using priceScale and timeScale without knowing what they were, so just randomly multiplying and dividing won't get me the result I want
+ok here is what I learned, so priceScale for example is calculated by dividing gridHeigh by priceRange, and I was just multiplying it with moved pixel, so the whole logic was backwards.
+Get it? cause I didn't.
+I went ahead and ask my boi claude and this was his response
+"This is a classic mistake that can only be made by someone who uses exactly 0% of their brain"
+"Your panning is fucked up because you're making several brain-dead mistakes with the scale calculations"
+
+so after bit back and forth I understood difference:
+so timeScale = gridWidth / priceRange which can be seen as timeScale = px / ms, see the picture
+scale is pixel per unit
+what we wanted was unit per pixel
+so timePerPixel = timeRange / gridWidth
+and that how we got this which now that I see make sense more and more.
+
+    const timeMoved = distanceMovedX * timePerPixel;
+    const priceMoved = distanceMovedY * pricePerPixel;
+
+well good for claude and bad for me, couldn't even code simple grid panning.
+
+ok now we have good grid and on grab and pan we can see the number change, but what we don't see is that feel that you are moving the chart, its more like grid is there and number on each axis change.
+I think because I still have lot of thing in for loop as static, because that makes it so grid won't change it's position in real chart it will , so have to do something about those steps.
+
+ok it was not the steps, it was more of fundamental issue, so what we have been doing is first drawing static grid line and just changing numbers but now what we have to do is to set up first nice price to start with then we have to do some magic equation that will give us position for each price, I have general idea how i do it but i can't say for sure but it would go something like from the first step which would be at bottom we check how far we are and so if we start with 100 and step is 120 we know we are 20 steps above right! so now we have price scale from which we will convert price into pixel and bata-bing-bata-boom it should work...
+it fuuuuuuuuuuucking worked, woooo yahooo, yeah ha, it fucking works
+let me do the same for the time and commit and go to sleep I fucking deserve a rest
+ok there were still some if condition problems but it works now
+![infinite_grid](infinite_grid.png)
+
+on next episode we will start with zoom in and out baby
--- src/modules/Chart/Chart.tsx
+++ src/modules/Chart/Chart.tsx
@@ -1,13 +1,17 @@
-import { useEffect, useRef, useState } from "react"
-import { ONE_HOUR, ONE_MINUTE, priceAxisWidth, theme, timeAxisHeight } from "./variables";
+import { useEffect, useRef, useState, type MouseEvent } from "react"
+import { ONE_DAY, ONE_MINUTE, priceAxisWidth, theme, timeAxisHeight } from "./variables";
 
 
 const Chart = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [minPrice, setMinPrice] = useState<number>(100);
   const [maxPrice, setMaxPrice] = useState<number>(200);
-  const [minTime, setMinTime] = useState<number>(Date.now() - (ONE_HOUR * 4));
+  const [minTime, setMinTime] = useState<number>(Date.now() - ONE_DAY);
   const [maxTime, setMaxTime] = useState<number>(Date.now());
+
+  const [lastX, setLastX] = useState(0);
+  const [lastY, setLastY] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
 
 
   const gridSize = 100;
@@ -28,47 +32,125 @@ const Chart = () => {
     ctx.font = "12px Arial";
     drawGrid(canvas, ctx)
 
+    window.addEventListener("mouseup",onMouseUp);
+
+    return ()=>{
+      window.removeEventListener("mouseup",onMouseUp);
+    };
+
   },[])
+
+  useEffect(()=>{
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+
+    if(!canvas || !ctx) return;
+    
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.fillStyle = theme.background;
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.strokeStyle = theme.grid;
+    ctx.lineWidth = 0.5;
+    ctx.font = "12px Arial";
+    drawGrid(canvas,ctx);
+
+  },[minPrice, maxPrice, minTime, maxTime])
 
   function drawGrid(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D){
     const priceRange = maxPrice - minPrice;
-    const stepSize = 10;
+    const timeRange = maxTime - minTime;
+
+    const timeStep = ONE_MINUTE * 30;
+    const priceStep = 10;
+
     const gridWidth = canvas.width - priceAxisWidth;
     const gridHeight = canvas.height - timeAxisHeight;
+
+    const priceScale = gridHeight / priceRange;
+    const timeScale = gridWidth / timeRange; 
 
     ctx.beginPath();
 
     // Vertical
-    let startingTime = minTime;
-    for(let x=0; x <= canvas.width; x += gridSize){
-      if(x >= gridWidth) break;
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, gridHeight);
-      ctx.fillStyle = theme.text;
-      ctx.fillText(new Date(startingTime).toLocaleTimeString("en-us", {hour12: false}), x - 20, gridHeight + 10);
-      startingTime += ONE_MINUTE * 30;
+    let startingTime = Math.ceil(minTime / timeStep) * timeStep;
+    for(let x=startingTime; x <= maxTime; x += timeStep){
+      const posX = (x - minTime) * timeScale;
+      if(posX >= 0 && posX <= gridWidth){
+        ctx.moveTo(posX, 0);
+        ctx.lineTo(posX, gridHeight);
+        ctx.fillStyle = theme.text;
+        const timeLabel = new Date(x).toLocaleTimeString("en-us", {hour12: false});
+        ctx.fillText(timeLabel.slice(0,5), posX - 15, gridHeight + 10);
+      }
     }
 
     // Horizontal
-    let startingPrice = maxPrice;
-    for(let y=0; y <= canvas.height; y += gridSize){
-      if(y >= gridHeight) break;
-      ctx.moveTo(0,y);
-      ctx.lineTo(gridWidth,y);
-      ctx.fillStyle = "#fff";
-      ctx.fillText(String(startingPrice), gridWidth + 10, y + 5);
-      startingPrice -= stepSize;
+    let startingPrice = Math.ceil(minPrice / priceStep) * priceStep;
+    for(let y=startingPrice; y <= maxPrice; y += priceStep){
+      const posY = gridHeight - (y - minPrice) * priceScale; 
+      if(posY >= 0 && posY <= gridHeight){
+        ctx.moveTo(0,posY);
+        ctx.lineTo(gridWidth,posY);
+        ctx.fillStyle = "#fff";
+        ctx.fillText(String(y), gridWidth + 10, posY + 5);
+      }
     }
     ctx.stroke();
     ctx.closePath();
 
+  };
+
+  function onMouseDown(e: MouseEvent){
+    setLastX(e.clientX);
+    setLastY(e.clientY);
+    setIsDragging(true);
   }
 
+  function onMouseMove(e: MouseEvent){
+    const canvas = canvasRef.current;
+
+    if(!canvas || !isDragging)return;
+
+    const distanceMovedX = e.clientX - lastX;
+    const distanceMovedY = e.clientY - lastY;
+
+    const newTimeRange =  maxTime - minTime;
+    const newPriceRange = maxPrice - minPrice;
+    const gridWidth = canvas.width - priceAxisWidth;
+    const gridHeight = canvas.height - timeAxisHeight;
+
+    const timePerPixel = newTimeRange / gridWidth;
+    const pricePerPixel = newPriceRange / gridHeight;
+
+    const timeMoved = distanceMovedX * timePerPixel;
+    const priceMoved = distanceMovedY * pricePerPixel;
+    
+    setMinTime(prev => prev - timeMoved);
+    setMaxTime(prev => prev - timeMoved);
+    setMinPrice(prev => prev + priceMoved);
+    setMaxPrice(prev => prev + priceMoved);
+
+    setLastX(e.clientX);
+    setLastY(e.clientY);
+
+  }
+
+  function onMouseUp(e: globalThis.MouseEvent){
+    setLastX(0);
+    setLastY(0);
+    setIsDragging(false);
+  }
   return (
     <div className="w-[100vw] h-[100vh] overflow-hidden p-5">
         <canvas
-          className="w-full h-full block border-2"
+          className={`w-full h-full block border-2 ${isDragging && "cursor-grabbing"}`}
           ref={canvasRef}
+          onMouseDown={onMouseDown}
+          onMouseMove={onMouseMove}
         ></canvas>
     </div>
 
